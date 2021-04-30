5f779b91f023a4af22ceb084b0623f88ce4f8972
Alberto Puentes <albertopuentes00@gmail.com>
2021-04-30 18:11:48 -0500

    More Join exercises update

--- more_join.py
+++ more_join.py
@@ -1,26 +1,33 @@
-1.
+# 1.
 # List the films where the yr is 1962 [Show id, title]
 
 SELECT id, title
  FROM movie
  WHERE yr=1962
 
- 2.
+#  2.
 # Give year of 'Citizen Kane'.
 
 select yr
 from movie
 where title='Citizen Kane'
 
-3.
+# 3.
 # List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
 select id, title, yr
 from movie
 where title like '%Star Trek%'
 order by yr
 
-4. 4.
-# What id number does the actor 'Glenn Close' have?
+# 4. 4.
+# # What id number does the actor 'Glenn Close' have?
 select actor.id
 from movie join actor on (movie.id=actor.id)
 where name = 'Glenn Close'
+
+# 5.
+# What is the id of the film 'Casablanca'
+
+select id
+from movie
+where title='Casablanca'
